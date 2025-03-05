522f185b6a8759fa4586562cb7e029de2f1f3b44
tanmaydixit27 <tanmaydixit7054@gmail.com>
2025-03-05 11:08:05 +0530

    updated api

--- src/api/formApi.js
+++ src/api/formApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/forms";
+const API_BASE_URL = "https://form-backend-1v23.onrender.com/api/forms";
 
 export const getForms = async () => axios.get(API_BASE_URL);
 
